aafb5a1ba195e68cb919cbbb0567be9c0030664f
thesolutionistx <solutionistservices@gmail.com>
2025-03-12 18:17:46 -0400

    react

--- dashboard/page.tsx
+++ dashboard/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import React from 'react';
 import { signOut, useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 
--- login/page.tsx
+++ login/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import React from 'react';
 import { signIn } from "next-auth/react";
 import { FormEvent, useState } from "react";
 import { useRouter } from "next/navigation";
